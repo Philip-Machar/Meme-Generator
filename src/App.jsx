f72afbb314ab8162fe6b27debbb78074ecd5c710
Philip-Machar <philipmachar788@gmail.com>
2024-10-15 16:34:20 +0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Card from "./components/Card";
+
+const App = () => {
+  return (
+    <div className="w-screen h-screen grid place-content-center bg-teal-100">
+      <Card />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+const Card = () => {
+  return (
+    <div className="w-[550px] text-white">
+      <div className="flex items-center bg-gradient-to-r from-[#672280] to-[#A626D3] px-8 py-6 gap-2">
+        <img className="w-[40px]" src="/images/trollFace.png" alt="troll face image" />
+        <div className="text-2xl font-bold">Meme Generator</div>
+      </div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Card
