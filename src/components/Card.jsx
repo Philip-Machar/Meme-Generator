e3396883d9de17769f722c74a3547ef62d80f056
Philip-Machar <philipmachar788@gmail.com>
2024-10-17 18:06:16 +0300

    Fifth commmit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,13 @@
+import memeData from "../memeData";
+
 const Card = () => {
+  const randomIndex = Math.floor(Math.random() * memeData.data.memes.length);
+  const meme = memeData.data.memes[randomIndex];
+
+  const handleClick = () => {
+    console.log(meme)
+  }
+
   return (
     <div className="w-[550px] bg-white shadow-xl">
       <div className="flex items-center bg-gradient-to-r from-[#672280] to-[#A626D3] px-8 py-6 gap-2">
@@ -6,7 +15,7 @@ const Card = () => {
         <div className="text-2xl text-white font-bold">Meme Generator</div>
       </div>
       <div className="mr-8 ml-8">
-        <form action="">
+        <div>
             <div className="flex gap-8 mt-8 mb-5">
                 <div className="grid gap-2 w-full">
                     <label htmlFor="topText">Top text</label>
@@ -17,8 +26,8 @@ const Card = () => {
                     <input className="w-full h-8 rounded-md outline-none border border-gray-300 pl-3 pr-3" id="bottomText" type="text" name="bottomText" />
                 </div>
             </div>
-            <button className="bg-gradient-to-r from-[#672280] to-[#A626D3] p-2 w-full rounded-md text-white mb-8" type="submit">Get a new meme image <img src="" alt="" /> </button>
-        </form>
+            <button onClick={handleClick} className="bg-gradient-to-r from-[#672280] to-[#A626D3] p-2 w-full rounded-md text-white mb-8">Get a new meme image <img src="" alt="" /> </button>
+        </div>
         <img className="w-full mb-10" src="/images/meme.png" alt="meme image" />
       </div>
     </div>
