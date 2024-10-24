0ee0d343bc1c80d6f5069019dfef7bec78fcd3cd
Philip-Machar <philipmachar788@gmail.com>
2024-10-24 09:05:28 +0300

    ninth commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,13 +2,27 @@ import memeData from "../memeData";
 import { useState } from "react";
 
 const Card = () => {
+  
+  const [allImages, setAllImages] = useState(memeData)
 
-  const [imageUrl, setImageUrl] = useState("/images/meme.png");
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "/images/meme.png"
+  })
 
-  const handleClick = () => {
+  const getMeme = () => {
     const randomIndex = Math.floor(Math.random() * memeData.data.memes.length);
-    const meme = memeData.data.memes[randomIndex];
-    setImageUrl(meme.url)
+    const randomMeme = allImages.data.memes[randomIndex];
+    setMeme(prevMeme => {
+      return (
+        {
+          ...prevMeme,
+          randomImage: randomMeme.url
+        }
+      )
+    })
+    console.log(randomMeme)
   }
 
   return (
@@ -29,9 +43,9 @@ const Card = () => {
                     <input className="w-full h-8 rounded-md outline-none border border-gray-300 pl-3 pr-3" id="bottomText" type="text" name="bottomText" />
                 </div>
             </div>
-            <button onClick={handleClick} className="bg-gradient-to-r from-[#672280] to-[#A626D3] p-2 w-full rounded-md text-white mb-8">Get a new meme image <img src="" alt="" /> </button>
+            <button onClick={getMeme} className="bg-gradient-to-r from-[#672280] to-[#A626D3] p-2 w-full rounded-md text-white mb-8">Get a new meme image <img src="" alt="" /> </button>
         </div>
-        <img className="w-full mb-10" src={imageUrl} alt="meme image" />
+        <img className="w-full mb-10" src={meme.randomImage} alt="meme image" />
       </div>
     </div>
   )
